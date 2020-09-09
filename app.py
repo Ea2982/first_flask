18d3c775c7572cab298cd160407f0b154a056e51
Leonardo <ea2982@gmail.com>
2020-09-09 15:43:21 +0300

    edit app.py add bottles.html add index.html

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template(
+        'index.html',
+        title='Hello, World!',
+        header='Welcome',
+        text='TODO'
+    )
+@app.route('/99-bottles')
+def bottles():
+    return render_template(
+        'bottles.html',
+        title='bottles'
+
+    )
 
 
 if __name__ == '__main__':
