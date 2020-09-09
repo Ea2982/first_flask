22012d20f413177b3b5b4ce002a5f372120e25f0
Leonardo <ea2982@gmail.com>
2020-09-09 16:42:28 +0300

    edit app.py add bottles.html add index.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__)
 
@@ -15,9 +15,19 @@ def bottles():
     return render_template(
         'bottles.html',
         title='bottles1'
-
     )
-
-
+@app.route('/echo')
+def echo():
+    print(request.path)
+    return request.path
+@app.route('/args', methods=['GET'])
+def args():
+    return render_template(
+        'args.html',
+        a=list(request.args.values())
+    )
+@app.route(('/div/<int:n1>/<int:n2>'))
+def div(n1, n2):
+    return str(n1 / n2)
 if __name__ == '__main__':
     app.run()
